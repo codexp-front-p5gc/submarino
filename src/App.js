39faa29ce05d3ea706323decfd40c617e10ac14a
Shirley Paz <shirley.pazz@gmail.com>
2017-12-14 15:49:51 -0200

    Sale HTM Semântico e APP.js

--- src/App.js
+++ src/App.js
@@ -1,136 +1,70 @@
 import React from "react";
-// import SectionProduct from "./components/SectionProduct/SectionProduct";
-import Notebook from "./img/notebook.jpg";
-import Chapinha from "./img/chapinha.jpg";
-import Smartphone from "./img/smartphone.png";
-import Assadeiras from "./img/assadeiras.jpg";
-import Aparador from "./img/aparador.png";
-import Livro from "./img/livro.jpg";
-import Smarttv from "./img/smarttv.png";
-import Game from "./img/game.png";
+import Sale from "./components/20-sale/sale";
+import TelevisaoSamsung from "./img/1200/TV-A.PNG";
+import SmartphoneMoto from "./img/1200/celular-F.PNG";
+import SmartphoneAsus from "./img/1200/celular-D.PNG";
+import NotebookDell from "./img/1200/notebook-A.PNG";
 import "./App.css";
-import SaleItem from "./components/21-saleitem/saleitem";
-import Televisao4k from "./img/1200/TV-A.PNG";
-import Prime from "./img/prime.png"
 
 
 var products = [
-    {
-        image: {
-            src: Chapinha, 
-            alt: "Chapinha Prancha"
-        },
-        name: "Chapinha Prancha Profissional Nano Titanium",
-        price: 42.99,
-        offer: true
-    },
-    {
-        image: {
-            src: Notebook, 
-            alt: "Notebook Acer"
-        },
-        name: "Notebook Acer A515-51G-58VH Intel Core I5 8GB",
-        price: 2999.99,
-        offer: true
-    },
-    {
-        image: {
-            src: Smartphone, 
-            alt: "Smartphone Motorola"
-        },
-        name: "Smartphone Motorola Moto G 5s Plus Dual Chip Android 7.1.1",
-        price: 1199.99,
-        offer: true
-    },
-    {
-        image: {
-            src: Assadeiras, 
-            alt: "Conjunto Assadeiras"
-        },
-        name: "Conjunto de Assadeiras de Vidro 10 Peças",
-        price: 139.99,
-        offer: true
-    },
-    {
-        name: "Kit Aparador de Pelos Philips Multigroom",
-        price: 88.99,
-        image: {
-            src: Aparador, 
-            alt: "Kit Aparador"
-        },
-        bestSeller: true
-    },
-    {
-        image: {
-            src: Livro, 
-            alt: "Livro Harry Potter"
-        },
-        name: "Livro Harry Potter e o Enigma do Príncipe",
-        price: 21.90,
-        bestSeller: true
-    },
-    {
-        image: {
-            src: Smarttv, 
-            alt: "Smart TV LED"
-        },
-        name: "Smart TV LED Curva 49 Samsung 49MU6300",
-        price: 2698.99,
-        bestSeller: true
-    },
-    {
-        image: {
-            src: Game, 
-            alt: "Game Call Of Duty"
-        },
-        name: "Game Call Of Duty: Infinite Warfare - PS4",
-        price: 39.99,
-        bestSeller: true
-    },
-];
 
-
-function bestSeller() {
-    var filtered = [];
-
-    for (var indice = 0; indice < products.length; indice++) {
-        if (products[indice].bestSeller) {
-            filtered.push(products[indice]);
+    {
+        image: {
+            src: TelevisaoSamsung,
+            alt: "Televisão 4K Samsung"
+        },
+        timer: "14:34:59",
+        title: "Smart TV Led 49'LG 49UJ6525 com Conversor",
+        price: 2499.00,
+        paymentOptions: {
+            installments: 10,
+            value: 263.05,
+        },
+    },
+    {
+        image: {
+            src: SmartphoneMoto,
+            alt: "Smartphone Motorola Moto X4 Dual Cam Android 7.0"
+        },
+        timer: "14:34:59",
+        title: "Smartphone Motorola Moto X4 Dual Cam Android 7.0",
+        price: 1231.99,
+        paymentOptions: {
+            installments: 10,
+            value: 139.99
+        }
+    },
+    {
+        image: {
+            src: NotebookDell,
+            alt: "Notebook Dell Inspiron i15 5567 D40B Inter Core 7 i7"
+        },
+        timer: "14:34:59",
+        title: "Notebook Dell Inspiron i15 5567 D40B Inter Core 7 i7",
+        price: 2659.99,
+        paymentOptions: {
+            installments: 10,
+            value: 279.99
+        }
+    },
+    {
+        image: {
+            src: SmartphoneAsus,
+            alt: "Smartphone Asus Zenfone 3 Dual Chip Android 6 Tela"
+        },
+        timer: "14:34:59",
+        title: "Smartphone Asus Zenfone 3 Dual Chip Android 6 Tela",
+        price: 1034.10,
+        paymentOptions: {
+            installments: 10,
+            value: 114.90
         }
     }
-
-    return filtered;
-}
-
-
-function offer() {
-    var filtered = [];
-
-    for (var indice = 0; indice < products.length; indice++) {
-        if (products[indice].offer) {
-            filtered.push(products[indice]);
-        }
-    }
-
-    return filtered;
-}
-
-var saleImage ={
-    src: Televisao4k,
-    alt: "Televisão 4K",
-}
-
-var product ={
-    title: "Smart TV Led 49'LG 49UJ6525 com Conversor ",
-    price: 2499.00,
-    paymentOptions: {
-        installments: 10 ,
-        value: 263.05
-    }
-}
+]
 
 export default () => (
-  <div className="saleitem">
-    <SaleItem image={saleImage} timer="14:34:59" title={product.title} price={product.price} paymentOptions={product.paymentOptions} />
-  </div>
+    <div className="App">
+        <Sale title="Sale Itens" />
+    </div>
 );
---
+++ src/components/20-sale/sale.js
@@ -0,0 +1,29 @@
+import React from "react";
+import SaleItem from "../21-saleitem/saleitem";
+import "./sale.css";
+
+
+function items(products) {
+    var components =  [];
+
+    for (var i = 0; i < products.length; i++) {
+        components.push(<SaleItem image={products[i].image} timer={products[i].timer} title={products[i].title} price={products[i].price} paymentOptions={products[i].paymentOptions} />);
+    }
+
+    return components;
+}
+
+export default (props) => (
+    <section className="section-sale">
+        <h1>{props.title}</h1>
+        <div className="section-sale__items">
+            {items(props.products)}
+        </div>
+    </section>
+);
+
+// sale-item-image - <img src={props.image.src} alt={props.image.alt}/>
+// sale-item-info__timer - {props.timer}
+// info-description__title - {props.title}
+// info-description-price - {props.price}
+// info-description__payment-options - {props.paymentOptions.installments}-{props.paymentOptions.value}
